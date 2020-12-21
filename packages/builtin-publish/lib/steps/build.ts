585a4eadb91906f5992444b456bc50bea3491eee
buns <kuafujs@126.com>
2020-12-21 17:57:19 +0800

    chore(builtin-publish): rm unused files

--- packages/builtin-publish/lib/steps/build.ts
+++
@@ -1,9 +0,0 @@
-import execa from "execa";
-
-import { PublishContext } from "../ctx";
-import { log } from "../utils";
-
-export async function build(ctx: PublishContext) {
-	log("\n Building packages...");
-	await execa("yarn", ["build"], { stdio: "inherit" });
-}
--- packages/builtin-publish/lib/steps/lint.ts
+++
@@ -1,9 +0,0 @@
-import execa from "execa";
-
-import { PublishContext } from "../ctx";
-import { log } from "../utils";
-
-export async function lint(ctx: PublishContext) {
-	log("\n Lint packages...");
-	await execa("yarn", ["test"], { stdio: "inherit" });
-}
