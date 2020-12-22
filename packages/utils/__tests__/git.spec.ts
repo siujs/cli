d4dbe36a752485d6ac75ab5ceee199563e08f0e7
buns <kuafujs@126.com>
2020-12-22 11:14:26 +0800

    test(utils): add `getFirstCommitId` case

--- packages/utils/__tests__/git.spec.ts
+++ packages/utils/__tests__/git.spec.ts
@@ -2,11 +2,23 @@ import fs from "fs-extra";
 import path from "path";
 import sh from "shelljs";
 
-import { downloadGit } from "../lib/git";
+import { downloadGit, getFirstCommitId } from "../lib/git";
+
+test(" getFirstCommitId ", async done => {
+	const expectedId = "289e90072966ebc2c549a01aab426ffd8b0940b3";
+
+	let id = await getFirstCommitId();
+	expect(id).toBe(expectedId);
+
+	id = await getFirstCommitId(true);
+	expect(id).toBe(expectedId.substring(0, 7));
+
+	done();
+});
 
 test("download Git", async done => {
 	const dest = path.resolve(__dirname, "./tpls");
-	await downloadGit("https://github.com/siujs/tpls", "master", dest);
+	await downloadGit("https://gitee.com/siujs/tpls", "master", dest);
 
 	let exists = fs.pathExistsSync(path.resolve(dest, "siu.config.js"));
 	expect(exists).toBe(true);
