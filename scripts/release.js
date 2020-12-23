94e6ab6ddaf482ef59e9b553d61685a9cc0062e0
buns <kuafujs@126.com>
2020-12-23 14:07:21 +0800

    chore: update release logic and change scripts.release

---
+++ scripts/release.js
@@ -0,0 +1,14 @@
+const minimist = require("minimist");
+const { release } = require("../packages/builtin-publish/dist/index");
+
+const cmd = minimist(process.argv);
+
+const skips = cmd.skip ? cmd.skip.split(",") : [];
+
+release({
+	dryRun: !!(cmd.dryRun || cmd["dry-run"]),
+	skipPush: skips.length && skips.includes("push"),
+	skipLint: skips.length && skips.includes("lint"),
+	skipBuild: skips.length && skips.includes("build"),
+	repo: cmd.repo
+});
