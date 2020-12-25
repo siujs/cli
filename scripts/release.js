a64a4fd55c2e81a13e574bf62fb2042bf3a2f9fe
buns <kuafujs@126.com>
2020-12-25 16:01:54 +0800

    chore: `skipLint` now default to true

--- scripts/release.js
+++ scripts/release.js
@@ -8,7 +8,7 @@ const skips = cmd.skip ? cmd.skip.split(",") : [];
 release({
 	dryRun: !!(cmd.dryRun || cmd["dry-run"]),
 	skipPush: skips.length && skips.includes("push"),
-	skipLint: skips.length && skips.includes("lint"),
+	skipLint: true,
 	skipBuild: skips.length && skips.includes("build"),
 	repo: cmd.repo
 });
