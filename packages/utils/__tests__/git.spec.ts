fbbc1bfddd69c0487033de039108610608f4eb92
buns <kuafujs@126.com>
2020-12-28 11:33:57 +0800

    test(utils): adjust test-case
    - add `getPreTag` case
    - change `getGitRemote`=>`getGitRemoteUrl`

--- packages/utils/__tests__/git.spec.ts
+++ packages/utils/__tests__/git.spec.ts
@@ -2,7 +2,7 @@ import fs from "fs-extra";
 import path from "path";
 import sh from "shelljs";
 
-import { downloadGit, getFirstCommitId, getGitRemote } from "../lib/git";
+import { downloadGit, getFirstCommitId, getGitRemoteUrl, getPreTag } from "../lib/git";
 
 test(" getFirstCommitId ", async done => {
 	const expectedId = "289e90072966ebc2c549a01aab426ffd8b0940b3";
@@ -31,10 +31,23 @@ test("download Git", async done => {
 	done();
 }, 600000);
 
-test(" getGitRemote ", async done => {
-	const url = await getGitRemote(process.cwd());
+test(" getGitRemoteUrl ", async done => {
+	const url = await getGitRemoteUrl(process.cwd());
 
 	expect(url).toBe("https://github.com/siujs/cli");
+
+	done();
+});
+
+test(" getPreTag ", async done => {
+	const tag = await getPreTag();
+
+	const { stdout } = sh.exec("git tag --list v* --sort -v:refname");
+
+	const tags = stdout.split("\n");
+
+	expect(!!tags).toBe(true);
+	expect(tag).toBe(tags[0]);
 
 	done();
 });
