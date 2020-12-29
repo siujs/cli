a0cba5e2b1042f93c0e30e1e8b4440522c05b843
buns <kuafujs@126.com>
2020-12-29 14:31:56 +0800

    feat(cli): add `skipPublish`、`skipCommit` option

--- packages/cli/lib/builtins/publish.ts
+++ packages/cli/lib/builtins/publish.ts
@@ -1,25 +1,31 @@
 import { release } from "@siujs/builtin-publish";
 import { HookHandlerContext, PluginApi, ValueOf } from "@siujs/core";
 
+interface PublishOptions {
+	pkg?: string;
+	ver?: string;
+	repo?: string;
+	dryRun?: boolean;
+	"dry-run"?: boolean;
+	skip?: string;
+}
+
 export function asPublishFallback(api: ValueOf<PluginApi>) {
 	api.process(async (ctx: HookHandlerContext) => {
-		const pkg = ctx.opts<string>("pkg");
-		const version = ctx.opts<string>("ver");
-		const repo = ctx.opts<string>("repo");
-		const dryRun = ctx.opts<boolean>("dryRun") || ctx.opts<boolean>("dry-run");
-
-		const skip = ctx.opts<string>("skip");
+		const { pkg, ver, repo, skip, ...dry } = ctx.opts<PublishOptions>();
 
 		const skips = skip ? skip.split(",") : [];
 
 		await release({
 			pkg,
-			version,
+			version: ver,
 			repo,
-			dryRun,
-			skipPush: !!skips.length && skips.includes("push"),
-			skipLint: !!skips.length && skips.includes("lint"),
-			skipBuild: !!skips.length && skips.includes("build")
+			dryRun: !!(dry.dryRun || dry["dry-run"]),
+			skipLint: !skips.length || skips.includes("lint"),
+			skipBuild: !!skips.length && skips.includes("build"),
+			skipPublish: !!skips.length && skips.includes("publish"),
+			skipCommit: !!skips.length && skips.includes("commit"),
+			skipPush: !!skips.length && skips.includes("push")
 		});
 	});
 }
