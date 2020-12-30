395478265a3fcaf073b8265fcbb4cc53bfb1b37b
buns <kuafujs@126.com>
2020-12-30 15:38:56 +0800

    fix(cli): ctx.opts('dep') => ctx.opts('deps')
    `dep` is not in `deps` cmd option, it cause undefined error

--- packages/cli/lib/builtins/deps.ts
+++ packages/cli/lib/builtins/deps.ts
@@ -3,6 +3,6 @@ import { HookHandlerContext, PluginApi, ValueOf } from "@siujs/core";
 
 export function asDepsFallback(api: ValueOf<PluginApi>) {
 	api.process(async (ctx: HookHandlerContext) => {
-		await changeDeps(ctx.opts<string>("pkg"), ctx.opts<string>("dep"), ctx.opts<"rm" | "add">("action"));
+		await changeDeps(ctx.opts<string>("pkg"), ctx.opts<string>("deps"), ctx.opts<"rm" | "add">("action"));
 	});
 }
